9062036c82d866af291dd6e9277c07c9ac733d60
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-08-29 17:03:51 -0300

    Estilizando lista de pedidos e utilizando component item de pedidos

--- src/pages/PurchaseList/index.js
+++ src/pages/PurchaseList/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
-import Header from '../../components/Header';
+import { View, ScrollView } from 'react-native';
 
-function PurchaseList() {
-  return (
-    <View>
-      <Header title={'Purchase List'} />
+import Header from '../../components/Header';
+import PurchaseItem from '../../components/PurchaseItem';
+
+import styles from './styles';
+
+function ClientsList() {
+
+  return(
+    <View style={styles.container}>
+      <Header title={'Pedidos do Cliente'} />
+      <ScrollView style={styles.purchaseList}>
+        <PurchaseItem />
+        <PurchaseItem />
+        <PurchaseItem />
+        <PurchaseItem />
+        <PurchaseItem />
+        <PurchaseItem />
+      </ScrollView>
     </View>
-  )
+  );
 }
 
-export default PurchaseList;
+export default ClientsList;
