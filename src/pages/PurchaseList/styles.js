f98ab47ea591b2d3a7c56e0041bcc1cc8168a5c3
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-08-29 19:07:45 -0300

    Alterações na estilização da lista de pedidos

--- src/pages/PurchaseList/styles.js
+++ src/pages/PurchaseList/styles.js
@@ -3,13 +3,13 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E2DFE2',
+    backgroundColor: '#BBE1FA',
   },
 
   purchaseList: {
     marginTop: -40,
     paddingHorizontal: 16,
-    paddingBottom: 16
+    marginBottom: 60
   }
 });
 
