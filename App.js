d1a7a04b8d9642bd592fba4db938d75b0e575cef
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-20 00:11:15 -0300

    Permitindo components terem acesso a Store e Redux-thunk como middleware

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import { createStore, applyMiddleware } from 'redux';
 import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import React from 'react';
 
 import AppStack from './src/routes/AppStack';
+import rootReducer from './src/reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
-    <>
-      <AppStack />
-      <StatusBar style="auto" />
-    </>
+      <Provider store={store}>
+        <AppStack />
+        <StatusBar style="auto" />
+      </Provider>
   );
 }
