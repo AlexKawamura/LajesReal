740cb87b0615b727db985f5fae9ecc07d3f4bef6
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-22 16:37:02 -0300

    Adicionando tela de cadastro de clientes na pilha

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../pages/Login';
 import ClientsList from '../pages/ClientsList';
 import ClientDetail from '../pages/ClientDetail';
+import ClientRegister from '../pages/ClientRegister';
 import PurchaseList from '../pages/PurchaseList';
 
 const { Navigator, Screen } = createStackNavigator();
@@ -13,10 +14,11 @@ function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="Login" component={Login} />
+        <Screen name="ClientRegister" component={ClientRegister} />
         <Screen name="ClientsList" component={ClientsList} />
         <Screen name="ClientDetail" component={ClientDetail} />
         <Screen name="PurchaseList" component={PurchaseList} />
+        <Screen name="Login" component={Login} />
       </Navigator>
     </NavigationContainer>
   );
