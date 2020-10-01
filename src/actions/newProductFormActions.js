3a28560b4455b59eec1caff7ced9bcc5b1a3d642
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-10-01 17:36:28 -0300

    Separando action setField por ser comum a todos os forms

--- src/actions/newProductFormActions.js
+++ src/actions/newProductFormActions.js
@@ -1,16 +1,9 @@
 import firebase from 'firebase';
 
-export const SET_FIELD_PRODUCTS = 'SET_FIELD_PRODUCTS';
-export const setField = (field, value) => {
-  return {
-    type: SET_FIELD_PRODUCTS,
-    field,
-    value
-  }
-}
-
 export const saveProduct = product => {
   return async dispatch => {
-    await firebase.database().ref(`/products`).push(product);
+    await firebase.database().ref(`/products`).push(product).then(() => {
+      console.log('Produto cadastrado com sucesso');
+    });
   };
 }
