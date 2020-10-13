7707a5b30cd1a1536735c2deca354becd5e43261
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-10-13 20:54:26 -0300

    Excluindo importação de arquivos de actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,4 @@
 export * from './userActions';
-export * from './newProductFormActions';
-export * from './newClientFormActions';
 export * from './setFormFieldActions';
 export * from './productsActions';
 export * from './clientsActions';
